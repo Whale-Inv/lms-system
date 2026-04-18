bc381d47551ce42c51bb98d7b068d48f1766a7e5
Nikita Dorozhko <nick.dorozhko@yandex.ru>
2026-04-18 15:35:36 +0400

    add gunicorn, readme file

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = "users.User"
